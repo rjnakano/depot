a250bc69677a29f88fa921a15455555b825999fb
Ryuma <rjnl_85@hotmail.com>
2015-03-20 16:08:09 -0500

    Create Depot project's MVC structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '8659fd1845ce34af306cecf417756b6efa1fa874ec80f29f1a72a5ec90b19f5dae4fb1b4fdab326a16507e542aa69bd482fca0aa6a4695778652c09c48bda2ca'
